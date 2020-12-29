faab8f060aac683a2f1bad5d9e84e939f53ba16c
Jenna <70181052+jennalala@users.noreply.github.com>
2020-12-29 16:51:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
-var startPage = document.getElementById("startPage");
-var questionsPage = document.getElementById("questionsPage");
-var highscoresPage = document.getElementById("highscoresPage");
-
-var startQuiz = document.getElementById("startQuiz");
-
 var index = 0;
+var timeLeft = 75;
+var interval;
+var score = 0;
+// var ids = []
 
-startPage.addEventListener("click", function () {
+startQuiz.addEventListener("click", init);
+enterYourName.addEventListener("click", insertName);
+view_highscores.addEventListener("click", linkViewHS)
+view_startpage.addEventListener("click", linkViewStartPage)
+
+function init() {
+  startTimer();
   startPage.classList.add("hide");
   questionsPage.classList.remove("hide");
- displayQuestion();
-});
+  displayQuestion();
+}
+
+function startTimer() {
+  clearInterval(interval);
+
+  interval = setInterval(function () {
+    timeLeft--;
+    seconds.textContent = timeLeft;
+
+    if (timeLeft === 0) {
+      endGame();
+    }
+  }, 1 * 1000);
+}
+
+// function startTimer() {
+//   interval = setInterval(function () {
+//     timeLeft--;
+//     seconds.textContent = timeLeft;
+//     console.log(ids)
+
+//     if(timeLeft === 0){
+//       for(const id of ids){
+//         clearInterval(id)
+//       }
+//     }
+
+//   }, 1 * 1000);
+
+//   ids.push(interval)
+// }
 
 function displayQuestion() {
- questionsPage.innerHTML = "";
+  questionsPage.innerHTML = "";
   var currentQ = myQuestions[index];
-
-  console.log(currentQ);
 
   var question = document.createElement("h3");
   question.textContent = currentQ.title;
@@ -24,31 +56,94 @@ function displayQuestion() {
   questionsPage.appendChild(question);
 
   for (var answer of currentQ.choices) {
-    console.log(answer);
     var btn = document.createElement("button");
     btn.textContent = answer;
     btn.onclick = check;
-
     questionsPage.appendChild(btn);
   }
 }
 
 function check() {
-//     // these aren't working..makes you unable to go to next question-figure out why
-// if (choices = answer)
-//    { "Correct"
-//  } else {
-//    "incorrect"
-// }
-    
-   // if (myQuestions = answer){
-    //   "correct!"
-    //  } else {
-   //     "incorrect"
-   //   }
+  if (myQuestions[index].answer === this.textContent) {
+    score++;
+  }
+  currentscore.textContent = score;
 
-//     // if clicked correct display correct
-  index++
-  displayQuestion();
+  index++;
+
+  if (index < myQuestions.length) {
+    displayQuestion();
+  } else {
+    endGame();
+  }
 }
 
+function endGame() {
+  clearInterval(interval);
+
+  finalScore.textContent = score;
+  questionsPage.classList.add("hide");
+  highscoresPage.classList.remove("hide");
+
+  displayHighScores();
+}
+
+function linkViewStartPage(){
+  view_highscores.classList.add("hide");
+  view_startpage.classList.remove("hide");
+  startPage.classList.remove("hide");
+  questionsPage.classList.add("hide");
+  highscoresPage.classList.add("hide");
+  scoreForm.classList.add("hide");
+}
+function linkViewHS(){
+  finalScore.textContent = score;
+  view_highscores.classList.add("hide");
+  view_startpage.classList.remove("hide");
+  startPage.classList.add("hide");
+  questionsPage.classList.add("hide");
+  highscoresPage.classList.remove("hide");
+  scoreForm.classList.add("hide");  
+  displayHighScores();
+}
+
+
+function insertName() {
+  var player = user_name.value;
+  scoreForm.classList.add("hide");
+
+  var record = { player, score };
+  var high_scores = getLS();
+  high_scores.push(record);
+  setLS(high_scores);
+
+  displayHighScores()
+}
+
+function displayHighScores() {
+  var high_scores = getLS();
+
+  var sorted_high_scores = high_scores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  for (const rec of sorted_high_scores) {
+    const li = document.createElement("li");
+    li.textContent = rec.player + ": " + rec.score;
+    highscore_list.appendChild(li);
+  }
+}
+
+var KEY = "HIGH_SCORES";
+function getLS() {
+  return JSON.parse(localStorage.getItem(KEY)) || [];
+}
+function setLS(data) {
+  localStorage.setItem(KEY, JSON.stringify(data));
+}
+
+
+var x = 'x'
+var y = 'y'
+
+console.log(x + ' - '+ y)
